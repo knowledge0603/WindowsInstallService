5993645a3f20d5b23dfd247b7efdf8a6bacf418a
knowledge0603 <23024084@qq.com>
2020-11-20 10:32:38 +0800

    “remark”

---
+++ installService/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (ServiceHelper.Status("test_service").ToString() == "NotExist")
+            {
+                //安装
+                ServiceHelper.Install(
+                    "test_service",                                // 服务名
+                    "test_service",                             // 显示名称
+                    @"""" + Application.ExecutablePath + @""" service",      // 映像路径，可带参数，若路径有空格，需给路径（不含参数）套上双引号
+                    "Frp Windows客户端",                         // 服务描述
+                    ServiceStartType.Auto,                 // 启动类型
+                    ServiceAccount.LocalSystem,           // 运行帐户，可选，默认是LocalSystem，即至尊帐户
+                    null      // 依赖服务，要填服务名称，没有则为null或空数组，可选
+                );
+                ServiceHelper.Restart("test_service");
+                WriteToFile("已注册到系统服务，并启动（控制台无输出）..." + "\r\n");
+            }
+            else
+            {
+                //卸载
+                ServiceHelper.Uninstall("test_service");
+                WriteToFile("已删除系统服务，因系统机制，如重新注册需重启本程序（或exploere.exe）..." + "\r\n");
+            }
+        }
+
+        public void WriteToFile(string Message)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\ServiceLog_form" + DateTime.Now.Date.ToShortDateString().Replace('/', '_') + ".txt";
+            if (!File.Exists(filepath))
+            {
+                // Create a file to write to. 
+                using (StreamWriter sw = File.CreateText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+        }
+        string serviceName = "ZookeeperWindowsService";
+        string serviceFilePath = AppDomain.CurrentDomain.BaseDirectory + "\\ZookeeperWindowsService.exe";
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (this.IsServiceExisted(serviceName))
+            {
+                this.UninstallService(serviceName);
+            }
+            this.InstallService(serviceFilePath) ;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (this.IsServiceExisted(serviceName)) this.ServiceStart(serviceName);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (this.IsServiceExisted(serviceName)) this.ServiceStop(serviceName);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (this.IsServiceExisted(serviceName))
+            {
+                this.ServiceStop(serviceName);
+                this.UninstallService(serviceFilePath);
+            }
+        }
+        private bool IsServiceExisted(string serviceName)
+        {
+            ServiceController[] services = ServiceController.GetServices();
+            foreach (ServiceController sc in services)
+            {
+                if (sc.ServiceName.ToLower() == serviceName.ToLower())
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        //安装服务
+        private void InstallService(string serviceFilePath)
+        {
+            using (AssemblyInstaller installer = new AssemblyInstaller())
+            {
+                installer.UseNewContext = true;
+                installer.Path = serviceFilePath;
+                IDictionary savedState = new Hashtable();
+                installer.Install(savedState);
+                installer.Commit(savedState);
+            }
+        }
+        //卸载服务
+        private void UninstallService(string serviceFilePath)
+        {
+            using (AssemblyInstaller installer = new AssemblyInstaller())
+            {
+                installer.UseNewContext = true;
+                installer.Path = serviceFilePath;
+                installer.Uninstall(null);
+            }
+        }
+        //启动服务
+        private void ServiceStart(string serviceName)
+        {
+            using (ServiceController control = new ServiceController(serviceName))
+            {
+                if (control.Status == ServiceControllerStatus.Stopped)
+                {
+                    control.Start();
+                }
+            }
+        }
+
+        //停止服务
+        private void ServiceStop(string serviceName)
+        {
+            using (ServiceController control = new ServiceController(serviceName))
+            {
+                if (control.Status == ServiceControllerStatus.Running)
+                {
+                    control.Stop();
+                }
+            }
+        }
+    }
+}
---
+++ installService/Service1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace WindowsFormsApp2
+{
+    partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            // TODO: 在此处添加代码以启动服务。
+            WriteToFile("Service is started at " + DateTime.Now);
+
+        }
+
+        protected override void OnStop()
+        {
+            // TODO: 在此处添加代码以执行停止服务所需的关闭操作。
+            WriteToFile("Service is stopped at " + DateTime.Now);
+        }
+
+        public void WriteToFile(string Message)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\ServiceLog_" + DateTime.Now.Date.ToShortDateString().Replace('/', '_') + ".txt";
+            if (!File.Exists(filepath))
+            {
+                // Create a file to write to. 
+                using (StreamWriter sw = File.CreateText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+        }
+    }
+}
